e19bd2cce969f569080deb5415e759cf588952e0
Brooke Yalof <brooke.yalof@mongodb.com>
2020-09-30 17:10:30 -0400

    nvm know your roots

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import ReactGA from "react-ga";
 import Main from "../components/Main";
+
 import styles from "../styles/Demo.module.css";
 
 export default function Home() {
@@ -15,6 +16,15 @@ export default function Home() {
   return (
     <div className={styles.main}>
       <Main />
+      <div className={styles.footer}>
+        <span>
+          We're you also disgusted by that debate? Great! You're in the right
+          place. Grab him by the ballot box! Vote blue no matter who! Brooke
+          loves you! Battle for the soul of the nation! Grab him by the ballot
+          box! Science is real! Black lives matter! No human is illegal! Love is
+          love! Women's rights are human rights! Kindness is everything!
+        </span>
+      </div>
     </div>
   );
 }
